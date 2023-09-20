5de763fc1322cd3d8c57d917d6772d39c8188a25
jl_ja <jl_ja@JEFERSON>
2023-09-20 20:52:15 -0300

    Add project files.

---
+++ GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,33 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MySQLContext>(options =>
+{
+    var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+    options.UseMySql(new MySqlServerVersion(new Version(8, 1, 0))); 
+
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
