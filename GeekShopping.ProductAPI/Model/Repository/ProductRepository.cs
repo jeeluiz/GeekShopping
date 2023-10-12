46e8f2ee997da52c24732b277b06b198f6696fd9
Jeferson . <jl_jau@hotmail.com>
2023-10-12 11:35:19 -0300

    Correcoes e adicao de produtos

--- GeekShopping.ProductAPI/Model/Repository/ProductRepository.cs
+++ GeekShopping.ProductAPI/Model/Repository/ProductRepository.cs
@@ -25,12 +25,34 @@ namespace GeekShopping.ProductAPI.Model.Repository
 
         }
 
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductVO>>(products);
+        }
+
+        public async Task<ProductVO> FindById(long id)
+        {
+            Product products = await _context.Products.Where(p => p.Id == id)
+            .FirstOrDefaultAsync() ?? new Product();
+
+            return _mapper.Map<ProductVO>(products);
+        }
+
+        public async Task<ProductVO> Update(ProductVO vO)
+        {
+            Product product = _mapper.Map<Product>(vO);
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
         public async Task<bool> DeleteById(long id)
         {
             try
             {
-                Product products = await _context.Products.Where(p => p.id == id)
-                    .FirstOrDefaultAsync();
+                Product products = await _context.Products.Where(p => p.Id == id)
+                    .FirstOrDefaultAsync() ?? new Product();
                 if (products == null) return false;
                 _context.Products.Remove(products);
                 await _context.SaveChangesAsync();
@@ -42,26 +64,6 @@ namespace GeekShopping.ProductAPI.Model.Repository
                 Console.WriteLine("asd");
                 return false;
             }
-        }
-
-        public async Task<IEnumerable<ProductVO>> FindAll()
-        {
-            List<Product> products = await _context.Products.ToListAsync();
-            return _mapper.Map<List<ProductVO>>(products);
-        }
-
-        public async Task<ProductVO> FindById(long id)
-        {
-            Product products = await _context.Products.Where(p => p.id == id).FirstOrDefaultAsync();
-            return _mapper.Map<ProductVO>(products);
-        }
-
-        public async Task<ProductVO> Update(ProductVO vO)
-        {
-            Product product = _mapper.Map<Product>(vO);
-            _context.Products.Update(product);
-            await _context.SaveChangesAsync();
-            return _mapper.Map<ProductVO>(product);
         }
     }
 
--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,7 @@
 using GeekShopping.ProductAPI.Model.Context;
 using GeekShopping.ProductAPI.Model.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI" }));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
