2ee1dd0152dfeccaffa7905688717544bbddfe1c
Jeferson . <jl_jau@hotmail.com>
2023-10-12 12:02:29 -0300

    Criandp a interface produto

---
+++ GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace GeekShopping.Web;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductModel>> FindAllProducts();
+    Task<ProductModel> FindProductsById(long id);
+    Task<ProductModel> CreateProduct(ProductModel model);
+    Task<ProductModel> UpdateProduct(ProductModel model);
+    Task<bool> DeleteProductById(long id);
+}
