784460fb1a76388f35c09673edc0bb2aaf991dd9
Jeferson . <jl_jau@hotmail.com>
2023-10-16 19:43:43 -0300

    configuracao de ingecao

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,7 +1,14 @@
+using GeekShopping.Web;
+using GeekShopping.Web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+c.BaseAddress = new Uri(builder.Configuration["ServiveUrls:ProductAPI"]));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
